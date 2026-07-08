6db4ed5a0375ef98d562c49f82b6d701e78692c0
Igrisv <zzzggzzzez@gmail.com>
2026-07-08 11:53:44 +0000

    Version semi estabe del server.

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ import billingRoutes, { handleStripeWebhook } from './routes/billing.js';
 import userRoutes from './routes/user.js';
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3001;
+
+// Request Logger
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
 // CORS
 const allowedOrigins = [
